319026ea5a5ba560142f9df0ad4b6c7f4eaf1c3f
Renan do Nascimento Sanches <renan.sanches_123@hotmail.com>
2020-06-18 23:29:20 -0300

    user typing, google login, chat fade

--- lib/components/input_button_in_fiap_ex.dart
+++ lib/components/input_button_in_fiap_ex.dart
@@ -3,26 +3,34 @@ import 'package:flutter/material.dart';
 class InputButtonFiapEx extends StatelessWidget {
   
   final String text;
-  InputButtonFiapEx(this.text);
+  final Color color;
+  final Widget leading;
+  InputButtonFiapEx(this.text, {this.leading, this.color});
 
   @override
   Widget build(BuildContext context) {
     return (Container(
-      width: 320.0,
+      padding: EdgeInsets.symmetric(horizontal:30),
       height: 60.0,
       alignment: FractionalOffset.center,
       decoration: BoxDecoration(
-        color: Theme.of(context).primaryColor,
+        color: color == null? Theme.of(context).primaryColor: color,
         borderRadius: BorderRadius.all(const Radius.circular(30.0)),
       ),
-      child: Text(
-        text,
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 20.0,
-          fontWeight: FontWeight.w500,
-          letterSpacing: 0.3,
-        ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          this.leading != null ? Padding(padding: EdgeInsets.only(right: 20), child:this.leading) : Container(),
+          Text(
+            text,
+            style: TextStyle(
+              color:  color != null? Theme.of(context).primaryColor: Colors.white,
+              fontSize: 20.0,
+              fontWeight: FontWeight.w500,
+              letterSpacing: 0.3,
+            ),
+          ),
+        ],
       ),
     ));
   }
